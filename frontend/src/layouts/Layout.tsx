8df9bdae80c5082259953475ec67fcc526a1e24e
piyush-3020 <piyush30rathwe@gmail.com>
2024-10-05 17:12:12 +0530

    search bar changes

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import SearchBar from '../components/searchBar'
+import SearchBar from "../components/SearchBar";
 
 interface Props {
   children: React.ReactNode;
